c55f33e79162420a3c5f3ab93ce295052c928270
Clayton <clay_21@outlook.com>
2025-03-05 05:44:17 +0400

    Learning loops Exercise XP Gold

--- Week 3/Day6/ExercisesXPGold/index.js
+++ Week 3/Day6/ExercisesXPGold/index.js
@@ -39,9 +39,16 @@
 let age = [20,5,12,43,98,55];
 
 // 1. Console.log the sum of all the numbers in the age array.
+// let sum = 0
+// for (let i=0; i < age.length ; i++){
+//     sum += age[i]
+// }
+// console.log ("sum of age:" , sum)
 
-
-// 2. Console.log the highest age in the array.
-
-
+// 2. Console.log the age in the array.
+ let highestAge = age[0]
+for (let i=1; i < age.length ; i++){
+   if (age[i] > highestAge){ highestAge =age[i]}
+}
+console.log ("Highest age:" , highestAge)
 
