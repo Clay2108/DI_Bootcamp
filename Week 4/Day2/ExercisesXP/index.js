46e9f8e27a21c98537815671969d02055e0948a7
Clayton <clay_21@outlook.com>
2025-03-06 06:08:16 +0400

    Update Exercise XP

--- Week 4/Day2/ExercisesXP/index.js
+++ Week 4/Day2/ExercisesXP/index.js
@@ -33,31 +33,86 @@
 // John created a simple tip calculator to help calculate how much to tip when he and his family go to restaurants.
 
 // Create a function named calculateTip() that takes no parameter.
-
-function calculateTip(){
-      const amount = prompt ("What is the amount of the bill?")
-  let tipPercent
-  if (amount <50) tipPercent =0.2
-  if (amount > 50 && amount < 200) tipPercent = 0.15
-  if (amount > 200) tipPercent = 0.1
-  const tip =amount *(1+tipPercent)
-  console.log("bill:" , amount)
-  console.log("Total bill incl tip:",tip)
-
-}
-calculateTip()
-
-// Inside the function, use prompt to ask John for the amount of the bill.
-
-// Here are the rules
+// calculateTip()
+//Inside the function, use prompt to ask John for the amount of the bill.
+//Here are the rules
 // If the bill is less than $50, tip 20%.
 // If the bill is between $50 and $200, tip 15%.
 // If the bill is more than $200, tip 10%.
 
 // Console.log the tip amount and the final bill (bill + tip).
 
+//Answer:
+// function calculateTip(){
+//       const amount = prompt ("What is the amount of the bill?")
+//   let tipPercent
+//   if (amount <50) tipPercent =0.2
+//   if (amount > 50 && amount < 200) tipPercent = 0.15
+//   if (amount > 200) tipPercent = 0.1
+//   const tip =amount *(1+tipPercent)
+//   console.log("bill:" , amount)
+//   console.log("Total bill incl tip:",tip)
+
+// }
+
+
 // Call the calculateTip() function.
 
 //----------------------------------------------------------------------------------------------------->
 // Exercise 3
 // Create a function call isDivisible() that takes no parameter.
+// In the function, loop through numbers 0 to 500.
+
+// Console.log all the numbers divisible by 23.
+
+// At the end, console.log the sum of all numbers that are divisible by 23.
+
+// function isDivisible(divisor){
+//   let sum = 0
+//   for (let i=0; i < 500 ; i++){
+//     if (i % divisor === 0) {
+//       console.log (i)
+//       sum = sum + i
+//     };
+//   }
+//   console.log ("Sum of number divisible by 20:" , sum)
+// }
+// isDivisible(20)
+
+//----------------------------------------------------------------------------------------------------->
+
+// Exercise 4:
+
+const stock = { 
+    "banana": 6, 
+    "apple": 0,
+    "pear": 12,
+    "orange": 32,
+    "blueberry":1
+}  
+
+const prices = {    
+    "banana": 4, 
+    "apple": 2, 
+    "pear": 1,
+    "orange": 1.5,
+    "blueberry":10
+} 
+
+// Add the stock and prices objects to your js file.
+
+// Create an array called shoppingList with the following items: “banana”, “orange”, and “apple”. It means that you have 1 banana, 1 orange and 1 apple in your cart.
+const shoppingList= ["banana", "orange" , "apple"];
+// Create a function called myBill() that takes no parameters.
+function myBill(){
+  let totalPrice=0
+  if(let i=0; i != stock ; i++)
+}
+
+// The function should return the total price of your shoppingList. In order to do this you must follow these rules:
+// The item must be in stock. (Hint : check out if .. in)
+// If the item is in stock find out the price in the prices object.
+
+// Call the myBill() function.
+
+// Bonus: If the item is in stock, decrease the item’s stock by 1
